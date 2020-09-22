7fdfcb4335710c37bed3b5cd1359fa2bdcd7c711
septian.arman009 <septian.arman009@gmail.com>
2020-09-22 20:34:11 +0700

    add dinamic dev and pro env

--- index.php
+++ index.php
@@ -53,8 +53,16 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	$server_ip = getHostByName(getHostName());
 
+	if (preg_match("/^(127\.0\.|10\.0\.).+/i", $server_ip)) {
+		define("ENVIRONMENT", "development");
+		define("BASEURL", "http://localhost:8000/");
+	} else {
+		define("ENVIRONMENT", "production");
+		define("BASEURL", "https://chics.zmedia.tech/");
+	}
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
